1ec10cad6b65399eed91223c356e0b8fe4fbdf8e
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-24 15:05:57 +0300

    added titles for pages

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -1,4 +1,8 @@
 <?php include '../includes/header.php' ?>
 <?php include '../includes/sidebar.php' ?>
-    <div class="pt-5">Dashboard</div>
+    <div class="pt-5">
+        <div class="title mt-1 mb-2">
+            <h1>Dashboard</h1>
+        </div>
+    </div>
 <?php include '../includes/footer.php' ?>
