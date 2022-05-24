4078adb1ef9ce077bdef84a79073c60ad87d8307
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-24 12:28:38 +0300

    added the list menu with icons

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,4 +1,34 @@
 <?php include_once 'includes/header.php' ?>
 
+<nav>
+    <div class="logo-name">Zomato</div>
+    <div class="menu-items">
+        <ul class="nav-links">
+            <li>
+                <i class="fa fa-tachometer" aria-hidden="true"></i>
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <i class="fa fa-cutlery" aria-hidden="true"></i>
+                <a href="#">Restaurants</a>
+            </li>
+            <li>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <a href="#">Reviews</a>
+            </li>
+            <li>
+                <i class="fa fa-users" aria-hidden="true"></i>
+                <a href="#">Users</a>
+            </li>
+        </ul>
+        <ul class="logout">
+            <li>
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+                <a href="#">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
 <?php include_once 'includes/footer.php' ?>
 
