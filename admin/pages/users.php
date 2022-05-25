5826101b69880717981bcda79c03deacc7617b08
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-25 21:26:55 +0300

    added users design page

--- admin/pages/users.php
+++ admin/pages/users.php
@@ -4,6 +4,207 @@
         <div class="title mt-1 mb-2">
             <h1>Users</h1>
         </div>
-
+    </div>
+    <div class="container users-loop">
+        <div class="mb-1">
+                <div class="restaurant d-flex">
+                    <div class="initial-name">
+                        <div>
+                            <span>A</span>
+                        </div>
+                    </div>
+                    <div class="pl-1 w-100">
+                        <div class="d-flex justify-space-between align-items">
+                            <div class="d-flex">
+                                <h3>Hasan Baba</h3>
+                            </div>
+                            <div>
+                                <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <p>Best Shawarma Restaurant in Saida</p>
+                    </div>
+                </div>
+            </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
+        <div class="mb-1">
+            <div class="restaurant d-flex">
+                <div class="initial-name">
+                    <div>
+                        <span>A</span>
+                    </div>
+                </div>
+                <div class="pl-1 w-100">
+                    <div class="d-flex justify-space-between align-items">
+                        <div class="d-flex">
+                            <h3>Hasan Baba</h3>
+                        </div>
+                        <div>
+                            <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <p>Best Shawarma Restaurant in Saida</p>
+                </div>
+            </div>
+        </div>
     </div>
 <?php include '../includes/footer.php' ?>
