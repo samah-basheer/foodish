74989bd0df3b67476da60f5448d4b5fdce634865
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-25 19:53:25 +0300

    added reviews section

--- admin/pages/reviews.php
+++ admin/pages/reviews.php
@@ -5,4 +5,70 @@
             <h1>Reviews</h1>
         </div>
     </div>
+    <div class="container reviews-loop">
+        <div class="row">
+            <div class="col-md-6 mb-1">
+                <div class="restaurant d-flex">
+                    <div class="initial-name">
+                        <div>
+                            <span>A</span>
+                        </div>
+                    </div>
+                    <div class="pl-1 w-100">
+                        <div class="d-flex justify-space-between align-items">
+                            <div class="d-flex">
+                                <h3>Hasan Baba</h3>
+                                <div class="review-stars">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </div>
+                            </div>
+                            <div>
+                                <i class="fa fa-check" aria-hidden="true"></i>
+                                <i class="fa fa-times" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <p>Best Shawarma Restaurant in Saida</p>
+                        <div class="text-right">
+                            <span class="restaurant-name">Abo Bahij</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-1">
+                <div class="restaurant d-flex">
+                    <div class="initial-name">
+                        <div>
+                            <span>A</span>
+                        </div>
+                    </div>
+                    <div class="pl-1 w-100">
+                        <div class="d-flex justify-space-between align-items">
+                            <div class="d-flex">
+                                <h3>Hasan Baba</h3>
+                                <div class="review-stars">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </div>
+                            </div>
+                            <div>
+                                <i class="fa fa-check" aria-hidden="true"></i>
+                                <i class="fa fa-times" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <p>Best Shawarma Restaurant in Saida</p>
+                        <div class="text-right">
+                            <span class="restaurant-name">Abo Bahij</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php include '../includes/footer.php' ?>
