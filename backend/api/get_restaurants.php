78e38d893c067cb61a503ded916c2c7479a65b1a
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-27 17:40:30 +0300

    added db connection

---
+++ backend/api/get_restaurants.php
@@ -0,0 +1,13 @@
+<?php
+include("../connection.php");
+$query = $mysqli->prepare("SELECT * from restaurants");
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($restaurant = $array->fetch_assoc()){
+    $response[] = $restaurant;
+}
+$json = json_encode($response);
+echo $json;
+
+?>
---
+++ backend/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = "root";
+$db_name = "sefdb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+?>
