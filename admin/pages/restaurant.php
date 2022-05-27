9c93aa587123614cd261d18c49d2cd0e5c593c15
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-28 01:00:01 +0300

    added restaurant loop

--- admin/pages/restaurant.php
+++ admin/pages/restaurant.php
@@ -5,6 +5,43 @@
             <h1>Restaurants</h1>
         </div>
     </div>
-    <div class="container restaurant-loop" id="restaurants"></div>
+    <div class="container restaurant-loop" id="restaurants">
+        <div class="row">
+        <?php
+        $jsonurl = "http://groupproject/backend/api/get_restaurants.php";
+        $json = file_get_contents($jsonurl);
+        $rest_arr = (json_decode($json));
+        $i = 0;
+
+        foreach ($rest_arr as $rest) {
+            if($i % 2 == 0) echo '</div><div class="row">';
+            ?>
+            <div class="col-md-6 mb-1">
+                <div class="restaurant d-flex">
+                    <div>
+                        <img src="../../assets/images/<?php echo $rest->pic_url ?>">
+                    </div>
+                    <div class="pl-1 w-100">
+                        <div class="d-flex justify-space-between align-items">
+                            <h3><?php echo $rest->name ?></h3>
+                            <div>
+                                <i class="fa fa-pencil-square-o edit-icon" aria-hidden="true"></i>
+                                <i class="fa fa-trash-o trash-icon" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <p><?php echo $rest->description ?></p>
+                        <div class="text-right">
+                            <span class="status">open now</span>
+                            <span>|</span>
+                            <span class="reviews">4.5 <i class="fa fa-star" aria-hidden="true"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            $i++;
+        }
+        ?>
+    </div>
 
 <?php include '../includes/footer.php' ?>
