9f743d2eb51a4c8f8ee42241ba25fca8878c3cd2
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-27 23:52:24 +0300

    fetched data from api and put it inside the restaurants page

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,6 @@
 </section>
 <!-- Scripts goes here -->
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="../assets/js/main.js"></script>
 </body>
 </html>
--- backend/api/get_restaurants.php
+++ backend/api/get_restaurants.php
@@ -1,8 +1,7 @@
 <?php
 include("../connection.php");
-$query = $mysqli->prepare("SELECT R.id, name, description, open_hr, close_hr, pic_url, location 
-FROM restaurants R, locations L 
-WHERE R.id = L.restaurant_id");
+$query = $mysqli->prepare("SELECT id, name, description, open_hr, close_hr, pic_url 
+FROM restaurants");
 $query->execute();
 $array = $query->get_result();
 $response = [];
