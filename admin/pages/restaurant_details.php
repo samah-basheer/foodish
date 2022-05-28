9df1c6b08da4919a38bd064fb0bafb149cc53051
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-28 14:20:36 +0300

    added edit restaurant page

--- admin/pages/restaurant_details.php
+++ admin/pages/restaurant_details.php
@@ -9,28 +9,29 @@ $rest_details = (json_decode($json));
 
 <div class="pt-5">
     <div class="container restaurant-detail">
-        <form>
+        <form action="../../backend/api/edit_restaurant.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $rest_details[0]->id; ?>">
             <div class="mb-2">
                 <label>Edit Restaurant Title</label>
-                <input type="text" value="<?php echo $rest_details[0]->name ?>">
+                <input type="text" name="name" value="<?php echo $rest_details[0]->name ?>">
             </div>
             <div class="row">
                 <div class="col-md-8">
                     <label>Edit Restaurant Description</label>
-                    <textarea rows="11"><?php echo $rest_details[0]->description ?></textarea>
+                    <textarea rows="11" name="description"><?php echo $rest_details[0]->description ?></textarea>
                     <div class="mb-1 mt-2">
                         <label>Edit Restaurant Open Hour</label>
-                        <input type="time" value="<?php echo $rest_details[0]->open_hr ?>">
+                        <input type="time" name="open_hr" value="<?php echo $rest_details[0]->open_hr ?>">
                     </div>
                     <div class="mb-1">
                         <label>Edit Restaurant Close Hour</label>
-                        <input type="time" value="<?php echo $rest_details[0]->close_hr ?>">
+                        <input type="time" name="close_hr" value="<?php echo $rest_details[0]->close_hr ?>">
                     </div>
                 </div>
                 <div class="col-md-4">
                     <label>Edit Restaurant Featured Image</label>
                     <img src="../../assets/images/<?php echo $rest_details[0]->pic_url ?>">
-                    <input type="file">
+                    <input type="file" name="pic_url">
                 </div>
                 <div class="col-md-12 text-right">
                     <div>
---
+++ backend/api/edit_restaurant.php
@@ -0,0 +1,27 @@
+<?php
+
+include("../connection.php");
+
+$name = $_POST["name"];
+$description = $_POST["description"];
+$open_hr = $_POST["open_hr"];
+$close_hr = $_POST["close_hr"];
+$pic_url = $_POST["pic_url"];
+$id = $_POST['id'];
+
+$query = $mysqli->prepare("UPDATE restaurants 
+SET name=?, description=?, open_hr=?, close_hr=?, pic_url=?
+WHERE id=?");
+$query->bind_param("sssssi", $name, $description, $open_hr, $close_hr, $pic_url, $id);
+$query->execute();
+
+$response = [];
+if($query->execute()) {
+    $response["success"] = true;
+} else {
+    $response["success"] = false;
+}
+echo json_encode($response);
+header('location: http://groupproject/admin/pages/restaurant_details.php?id=7');
+
+?>
