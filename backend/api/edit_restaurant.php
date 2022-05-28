3c3e5e4b745c7bfe7c921e3996b36c40c9556b30
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-28 18:11:01 +0300

    added file upload functionality

--- backend/api/edit_restaurant.php
+++ backend/api/edit_restaurant.php
@@ -7,7 +7,23 @@ $name = $_POST["name"];
 $description = $_POST["description"];
 $open_hr = $_POST["open_hr"];
 $close_hr = $_POST["close_hr"];
-$pic_url = $_FILES["pic_url"]["name"];
+if(!empty($_FILES["pic_url"]["name"])) {
+    $pic_url = $_FILES["pic_url"]["name"];
+    $target_dir = "../../assets/images/";
+    $target_file = $target_dir . basename($_FILES["pic_url"]["name"]);
+    move_uploaded_file($_FILES["pic_url"]["tmp_name"], $target_file);
+} else {
+    $query = $mysqli->prepare("SELECT pic_url
+    FROM restaurants
+    WHERE id=?");
+    $query->bind_param("i", $id);
+    $query->execute();
+    $array = $query->get_result();
+    $response = [];
+    $restaurant = $array->fetch_assoc();
+    $response[] = $restaurant;
+    $pic_url = $response[0]['pic_url'];
+}
 
 $query = $mysqli->prepare("UPDATE restaurants 
 SET name=?, description=?, open_hr=?, close_hr=?, pic_url=?
