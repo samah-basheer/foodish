0a7ee8a44ab8286acb9af687c039cd0de94ff9b6
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-28 09:24:57 +0300

    added delete restaurant api

---
+++ backend/api/delete_restaurant.php
@@ -0,0 +1,20 @@
+<?php
+
+include("../connection.php");
+
+$id = $_POST["id"];
+
+$query = $mysqli->prepare("DELETE FROM restaurants WHERE id=?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$response = [];
+if($query->execute()) {
+    $response["success"] = true;
+} else {
+    $response["success"] = $query->error;
+
+}
+echo json_encode($response);
+
+?>
