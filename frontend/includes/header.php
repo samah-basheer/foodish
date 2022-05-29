12e6780ff382e62bd65cb642c588f92757880cb2
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-29 13:27:46 +0300

    added frontend structure

---
+++ frontend/includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FOODISH</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="icon" type="image/x-icon" href="../assets/images/foodish-logo.png">
+</head>
+<body>
+<nav class="navbar">
+    <div class="nav-content">
+        <div class="logo">
+            <a href="/">
+                <img src="../assets/images/foodish-logo.png" style="max-width: 50px;">
+                <span style="margin-left: 12px;">FOODISH</span>
+            </a>
+        </div>
+        <ul class="menu-list">
+            <div class="icon cancel-btn">
+                <i class="fas fa-times"></i>
+            </div>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Resturants</a></li>
+            <li><a href="#">Rating</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="icon menu-btn">
+            <i class="fas fa-bars"></i>
+        </div>
+    </div>
+</nav>
+<div class="banner"></div>
---
+++ frontend/pages/home.php
@@ -0,0 +1,93 @@
+<?php include_once '../includes/header.php'; ?>
+    <div class="about">
+        <div class="image-sec">
+            <img src="../assets/images/about-bg-6-2.png" alt="" />
+        </div>
+        <div class="text-sec">
+            <h1>Discover Our <span style="color: #e63946">Story</span></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+                commodi doloribus natus. Distinctio rerum repellendus incidunt magni
+                molestias sapiente, amet cumque commodi aspernatur corporis nulla
+                tempore qui facere veniam rem ea assumenda!
+            </p>
+        </div>
+    </div>
+
+    <div class="our-resturants">
+        <div class="content">
+            <div class="p-slider">
+                <h1 class="resturant-slider-header">Top Resturants</h1>
+                <div class="slides">
+                    <div class="resturant-box">
+                        <div class="p-img-container">
+                            <div class="p-img">
+                                <a href="">
+                                    <img src="../assets/images/malek.jpg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="p-box-text">
+                            <p class="resturant-name">
+                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
+                                <span class="rest-status">open</span>
+                            </p>
+                            <hr class="rest-seperator" />
+                            <p class="resturant-description">
+                                Malak al Tawouk Restaurant is the most popular Tawouk
+                                restaurant chain, specializing in original recipes, providing
+                                Professional management and offering great service.
+                            </p>
+                        </div>
+                        <div class="returant-view"></div>
+                    </div>
+                    <div class="resturant-box">
+                        <div class="p-img-container">
+                            <div class="p-img">
+                                <a href="">
+                                    <img src="../assets/images/malek.jpg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="p-box-text">
+                            <p class="resturant-name">
+                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
+                                <span class="rest-status">open</span>
+                            </p>
+                            <hr class="rest-seperator" />
+                            <p class="resturant-description">
+                                Malak al Tawouk Restaurant is the most popular Tawouk
+                                restaurant chain, specializing in original recipes, providing
+                                Professional management and offering great service.
+                            </p>
+                        </div>
+                        <div class="returant-view"></div>
+                    </div>
+                    <div class="resturant-box">
+                        <div class="p-img-container">
+                            <div class="p-img">
+                                <a href="">
+                                    <img src="../assets/images/malek.jpg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="p-box-text">
+                            <p class="resturant-name">
+                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
+                                <span class="rest-status">open</span>
+                            </p>
+                            <hr class="rest-seperator" />
+                            <p class="resturant-description">
+                                Malak al Tawouk Restaurant is the most popular Tawouk
+                                restaurant chain, specializing in original recipes, providing
+                                Professional management and offering great service.
+                            </p>
+                        </div>
+                        <div class="returant-view"></div>
+                    </div>
+                </div>
+                <button class="resturants-view">View All</button>
+            </div>
+        </div>
+    </div>
+<?php include_once '../includes/footer.php'; ?>
