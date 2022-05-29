f7e17a60d437272b80ed2edbf4601895d77bcaf8
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-29 21:09:57 +0300

    added restaurant loop in homepage

--- frontend/pages/home.php
+++ frontend/pages/home.php
@@ -21,72 +21,34 @@ include_once '../includes/header.php';
             <div class="p-slider">
                 <h1 class="resturant-slider-header">Top Resturants</h1>
                 <div class="slides">
+                    <?php
+                    $jsonurl = "http://groupproject/backend/api/get_restaurants.php";
+                    $json = file_get_contents($jsonurl);
+                    $rest_arr = (json_decode($json));
+
+                    foreach (array_slice($rest_arr, 0, 3) as $rest) {
+                    ?>
                     <div class="resturant-box">
                         <div class="p-img-container">
                             <div class="p-img">
                                 <a href="">
-                                    <img src="../assets/images/malek.jpg" alt="" />
+                                    <img src="../assets/images/<?php echo $rest->pic_url; ?>" alt="" />
                                 </a>
                             </div>
                         </div>
                         <div class="p-box-text">
                             <p class="resturant-name">
-                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
+                                <a href="#" class="rest-title"><span><?php echo $rest->name; ?></span></a>
                                 <span class="rest-status">open</span>
                             </p>
                             <hr class="rest-seperator" />
                             <p class="resturant-description">
-                                Malak al Tawouk Restaurant is the most popular Tawouk
-                                restaurant chain, specializing in original recipes, providing
-                                Professional management and offering great service.
+                                <?php echo $rest->description; ?>
                             </p>
                         </div>
                         <div class="returant-view"></div>
                     </div>
-                    <div class="resturant-box">
-                        <div class="p-img-container">
-                            <div class="p-img">
-                                <a href="">
-                                    <img src="../assets/images/malek.jpg" alt="" />
-                                </a>
-                            </div>
-                        </div>
-                        <div class="p-box-text">
-                            <p class="resturant-name">
-                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
-                                <span class="rest-status">open</span>
-                            </p>
-                            <hr class="rest-seperator" />
-                            <p class="resturant-description">
-                                Malak al Tawouk Restaurant is the most popular Tawouk
-                                restaurant chain, specializing in original recipes, providing
-                                Professional management and offering great service.
-                            </p>
-                        </div>
-                        <div class="returant-view"></div>
-                    </div>
-                    <div class="resturant-box">
-                        <div class="p-img-container">
-                            <div class="p-img">
-                                <a href="">
-                                    <img src="../assets/images/malek.jpg" alt="" />
-                                </a>
-                            </div>
-                        </div>
-                        <div class="p-box-text">
-                            <p class="resturant-name">
-                                <a href="#" class="rest-title"><span>Malek Taouk</span></a>
-                                <span class="rest-status">open</span>
-                            </p>
-                            <hr class="rest-seperator" />
-                            <p class="resturant-description">
-                                Malak al Tawouk Restaurant is the most popular Tawouk
-                                restaurant chain, specializing in original recipes, providing
-                                Professional management and offering great service.
-                            </p>
-                        </div>
-                        <div class="returant-view"></div>
-                    </div>
+                    <?php } ?>
                 </div>
                 <button class="resturants-view">View All</button>
             </div>
